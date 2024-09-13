0715749e846891361422d0083f78ddf1d8666e5b
Isaac Haar <iah871839@gmail.com>
2024-09-13 14:09:17 -0400

    first commit

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "iah11";
+    char STUDENT_ID[] = "965825006";
 
     return 0 ;
 }
